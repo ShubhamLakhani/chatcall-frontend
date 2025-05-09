b92882acdaf675bf3179a7befd176acb0e40c547
ShubhamLakhani <lakhanishubham9750@gmail.com>
2025-05-10 01:10:32 +0530

    removed dynamic

--- src/app/call/page.tsx
+++ src/app/call/page.tsx
@@ -1,7 +1,6 @@
-import dynamic from 'next/dynamic';
 import { Suspense } from 'react';
+import CallClient from '~/components/call/CallClient';
 
-const CallClient = dynamic(() => import('~/components/call/CallClient'), { ssr: false });
 
 export default function CallPage() {
   return (
