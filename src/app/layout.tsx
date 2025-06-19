0273ce1d7567ccbe473e3b17a40c0f743bbb1251
ShubhamLakhani <lakhanishubham9750@gmail.com>
2025-06-19 15:43:32 +0530

    fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,6 @@ export default function RootLayout({
                 s1.async = true;
                 s1.src = "https://self-booking.happypet.tech/widget.js";
                 s1.charset = "UTF-8";
-                s1.setAttribute("crossorigin", "*");
                 s0.parentNode.insertBefore(s1, s0);
               })();
             `,
